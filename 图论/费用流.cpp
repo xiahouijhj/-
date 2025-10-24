67b8fee1bf4d8cb1133c991b9eeaa310ae8cc1a5
xiahouijhj <120567551+xiahouijhj@users.noreply.github.com>
2025-10-24 14:17:23 +0800

    Create 费用流.cpp

---
+++ 图论/费用流.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int inf=1e13+10;
+const int M=1e5+10,N=1e4+10;
+int n,m,s,t;
+struct stu
+{
+	int v,next,w;
+	int val;
+}e[M];
+int head[N],cnt=1,dis[N],vis[N];
+int mia=0;
+inline void add(int u,int v,int w,int ww)
+{
+	e[++cnt].next=head[u];
+	e[cnt].v=v;
+	e[cnt].val=w;
+	e[cnt].w=ww;
+	head[u]=cnt;
+}
+queue<int> q;
+inline int bfs()
+{
+	for(int i=1;i<=n;i++) vis[i]=0;
+	for(int i=0;i<=n;i++) dis[i]=inf;
+	q.push(s);
+	vis[s]=1;
+	dis[s]=0;
+	while(!q.empty())
+	{
+        int u=q.front();
+		q.pop();
+		vis[u]=0;
+		for(int i=head[u];i!=-1;i=e[i].next)
+		{
+            int v=e[i].v;
+			if(dis[u]+e[i].w<dis[v]&&e[i].val>0)
+			{
+				dis[v]=dis[u]+e[i].w;
+				if(vis[v]==0)
+				{
+					q.push(v);
+					vis[v]=1;
+				}
+			}
+		}
+	}
+    // cout<<t<<' '<<dep[t]<<endl;
+	if(dis[t]!=inf) return 1;
+	else return 0;
+}
+inline int dfs(int u,int in)
+{
+	long long out=0;
+	if(t==u) return in;
+	vis[u]=1;
+	for(int i=head[u];i!=-1&&in;i=e[i].next)
+	{
+		int v=e[i].v;
+		if(vis[v]==1||e[i].val==0||dis[v]!=dis[u]+e[i].w) continue;
+		long long k=dfs(v,min(in,e[i].val));
+		out=out+k;
+		in=in-k;
+		e[i].val-=k;
+		e[i^1].val+=k;
+		mia+=k*e[i].w;
+	}
+	if(out==0) dis[u]=inf;
+	return out;
+}
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    cin>>n>>m>>s>>t;
+    for(int i=0;i<=n;i++) head[i]=-1;
+	for(int i=1;i<=m;i++)
+	{
+		int x,y,w,b;
+		cin>>x>>y>>w>>b;
+		add(x,y,w,b);
+		add(y,x,0,-b);
+	}
+	long long ans=0;
+    while(bfs())
+    {
+		for(int i=1;i<=n;i++) vis[i]=0;
+        ans+=dfs(s,inf);
+    }
+	cout<<ans<<' '<<mia;
+} 
+ 
