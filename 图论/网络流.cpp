6280c292d6a1ed4bae19c2547b4f14984d1353df
xiahouijhj <120567551+xiahouijhj@users.noreply.github.com>
2025-10-24 15:20:29 +0800

    Update 网络流.cpp

--- 图论/网络流.cpp
+++ 图论/网络流.cpp
@@ -1,13 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int M=6e6,N=300000; 
+// #define int long long
+const int M=6e6,N=300000;
+const int inf=1e9+10;
+const int maxn=1e3+100;
+const int B=100;
+int a[maxn][B];
+int cur[maxn];
+int b[B];
 int n,m,s,t;
+template <class T>
+inline void read(T &x) {
+    x = 0;
+    char c = getchar();
+    bool f = 0;
+    for (; !isdigit(c); c = getchar()) f ^= c == '-';
+    for (; isdigit(c); c = getchar()) x = x * 10 + (c ^ 48);
+    x = f ? -x : x;
+}
+
+template <class T>
+inline void write(T x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    T y = 1;
+    int len = 1;
+    for (; y <= x / 10; y *= 10) ++len;
+    for (; len; --len, x %= y, y /= 10) putchar(x / y + 48);
+}
 struct stu
 {
 	int v,next;
 	long long val;
 }e[M];
-int head[N],cnt=1,dep[N];
+int head[maxn],cnt=1,dep[maxn];
 inline void add(int u,int v,long long w)
 {
 	e[++cnt].next=head[u];
@@ -15,67 +43,103 @@ inline void add(int u,int v,long long w)
 	e[cnt].val=w;
 	head[u]=cnt;
 }
-inline int bfs()
+inline bool bfs()
 {
+    for(int i=s;i<=t;i++) cur[i]=head[i];
 	queue<int> q;
-	for(int i=1;i<=n;i++)
-	{
-		dep[i]=0;
-	}
+    memset(dep,0,sizeof(dep));
 	q.push(s);
 	dep[s]=1;
 	while(!q.empty())
 	{
 		int u=q.front();
 		q.pop();
-		for(int i=head[u];i!=-1;i=e[i].next)
+		for(int i=head[u];i!=0;i=e[i].next)
 		{
 			int v=e[i].v;
 			if(dep[v]==0&&e[i].val>0)
 			{
 				dep[v]=dep[u]+1;
+                if(v==t) return true;
 				q.push(v);
 			}
 		}
 	}
-	if(dep[t]!=0) return 1;
-	else return 0;
+	// if(dep[t]!=0) return 1;
+    return false;
 }
 inline int dfs(int u,long long in)
 {
 	long long out=0;
 	if(t==u) return in;
-	for(int i=head[u];i!=-1&&in;i=e[i].next)
+	for(int i=cur[u];i!=0&&in;i=e[i].next)
 	{
+        cur[u]=i;
 		int v=e[i].v;
 		if(e[i].val==0||dep[v]!=dep[u]+1) continue;
 		long long k=dfs(v,min(in,e[i].val));
-		out=out+k;
+        out=out+k;
 		in=in-k;
 		e[i].val-=k;
 		e[i^1].val+=k;
+        if(in==0) break;
 	}
 	if(out==0) dep[u]=0;
 	return out;
 }
-int main()
+inline int check(int le)
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
-    cin>>n>>m>>s>>t;
-	for(int i=1;i<=m;i++) head[i]=-1;
-	for(int i=1;i<=m;i++)
-	{
-		int u,v,w;
-		cin>>u>>v>>w;
-		add(u,v,w);    
-		add(v,u,0);
-	}
-	long long ans=0;
-	while(bfs())
-	{
-		ans+=dfs(s,1234567890);
-	}
-	cout<<ans;
+    for(int i=1;i+le-1<=m;i++)
+    {
+        int ans=0;
+        s=0;t=n+m+1;
+        memset(head,0,sizeof(head));
+        cnt=1;
+        for(int j=1;j<=n;j++)
+        {
+            add(s,j,1);add(j,s,0);
+        }
+        for(int j=n+1;j<=n+m;j++)
+        {
+            add(j,t,b[j-n]);add(t,j,0);
+        }
+        for(int j=1;j<=n;j++)
+        {
+            for(int k=i;k<=i+le-1;k++) add(j,n+a[j][k],1),add(n+a[j][k],j,0);
+        }
+        for(;(bfs());)
+        {
+            ans+=dfs(s,inf);
+        }
+        if(ans>=n) return true;
+        // cout<<ans<<' '<<
+    }
+    return false;
+}
+signed main()
+{
+    read(n);read(m);
+    // ios::sync_with_stdio(false);
+    // cin.tie(0);cout.tie(0);
+    // cin>>n>>m;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            read(a[i][j]);
+        }
+    }
+    for(int i=1;i<=m;i++) read(b[i]);
+    int l=1,r=m;
+    while(l<r)
+    {
+        int mid=(l+r)>>1;
+        if(check(mid))
+        {
+            r=mid;
+        }
+        else l=mid+1;
+    }
+    write(l);putchar('\n');
  } 
  
