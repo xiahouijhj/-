bcdb68f2c3694ab8b7312ad64e289cf5701d3e01
xiahouijhj <120567551+xiahouijhj@users.noreply.github.com>
2025-12-26 17:07:45 +0800

    Update 线性乘法逆元.cpp

--- 数学/线性乘法逆元.cpp
+++ 数学/线性乘法逆元.cpp
@@ -1,3 +1,14 @@
 inv[1] = 1;
 for(int i = 2; i < p; ++ i)
     inv[i] = (p - p / i) * inv[p % i] % p;
+
+// O(n+logP) 离线计算多个逆元
+for(int i=1;i<=k;i++) p[i]=c(p[i-1],x[i]);fp[k]=fast(p[k],mod-2);
+for(int i=k-1;i>=0;i--)
+{
+    fp[i]=c(fp[i+1],x[i+1]);
+}
+for(int i=1;i<=k;i++)
+{
+    fx[i]=c(fp[i],p[i-1]);
+}
