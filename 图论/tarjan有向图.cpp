9754b8148f9d75ad845b4c32f88797e744d28afa
xiahouijhj <120567551+xiahouijhj@users.noreply.github.com>
2026-03-14 17:06:16 +0800

    Update tarjan有向图.cpp

--- 图论/tarjan有向图.cpp
+++ 图论/tarjan有向图.cpp
@@ -1,46 +1,45 @@
-struct Tarjan
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn=100003;
+int cnt=1;
+struct node
 {
-    static const int maxn=100003;
-    int cnt=1;
-    struct node
+    int u,v,w,next;
+}e[maxn];
+int low[maxn],dfn[maxn],sta[maxn],sd[maxn],vis[maxn],head[maxn];
+int t=1,hh=1;
+inline void add(int u,int v)
+{
+    e[++cnt].v=v;
+    e[cnt].u=u;
+    e[cnt].next=head[u];
+    head[u]=cnt;
+}
+inline void tarjan(int x)
+{
+    low[x]=dfn[x]=++t;
+    sta[++hh]=x;vis[x]=1;
+    for(int i=head[x];i;i=e[i].next)
     {
-        int u,v,w,next;
-    }e[maxn];
-    int low[maxn],dfn[maxn],sta[maxn],sd[maxn],vis[maxn],head[maxn];
-    int t=1,hh=1;
-    inline void add(int u,int v)
-    {
-        e[++cnt].v=v;
-        e[cnt].u=u;
-        e[cnt].next=head[u];
-        head[u]=cnt;
-    }
-    inline void tarjan(int x)
-    {
-        low[x]=dfn[x]=++t;
-        sta[++hh]=x;vis[x]=1;
-        for(int i=head[x];i;i=e[i].next)
+        int v=e[i].v;
+        if(!dfn[v])
         {
-            int v=e[i].v;
-            if(!dfn[v])
-            {
-                tarjan(v);
-                low[x]=min(low[x],low[v]);
-            }
-            else if(vis[v])
-            {
-                low[x]=min(low[x],low[v]);
-            }
+            tarjan(v);
+            low[x]=min(low[x],low[v]);
         }
-        if(dfn[x]==low[x])
+        else if(vis[v])
         {
-            int y;
-            while(y=sta[hh--])
-            {
-                sd[y]=x;
-                vis[y]=0;
-                if(x==y) break;
-            }
+            low[x]=min(low[x],low[v]);
         }
     }
-};
+    if(dfn[x]==low[x])
+    {
+        int y;
+        while(y=sta[hh--])
+        {
+            sd[y]=x;
+            vis[y]=0;
+            if(x==y) break;
+        }
+    }
+}
