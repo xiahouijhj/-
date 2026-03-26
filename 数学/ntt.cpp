603d136f323ec36925250308a6abfed136604d1a
xiahouijhj <120567551+xiahouijhj@users.noreply.github.com>
2026-03-26 16:01:02 +0800

    Update ntt.cpp

--- 数学/ntt.cpp
+++ 数学/ntt.cpp
@@ -1,96 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
+#define ll long long
 const int MAXN=3*1e6+10;
-const int mod=998244353,G=3,Gi=332748118;//绝大多数情况下如此
+int n,m,limit=1,L,r[MAXN];
+const long long N=1010101;
+const long long inf=0x3f3f3f3f;
+const long long p=998244353, g=3, gi=332748118;
 // mod = 998244353、1004535809、469762049
 // Gi = 332748118, 334845270, 156587350
-int n,m,limit=1,L,r[MAXN];
-long long a[MAXN],b[MAXN];
-long long fastpow(long long a, long long k)
-{
-    long long res=1ll;
-    while(k)
-    {
-        if(k&1)
-        {
-            res=(res*a)%mod;
-        }
-        a=(a*a)%mod;
-        k>>=1ll;
-    }
-    return res%mod;
+ll Len;
+
+inline ll read() {
+	ll sum = 0, ff = 1; char c = getchar();
+	while(c<'0' || c>'9') { if(c=='-') ff = -1; c = getchar(); }
+	while(c>='0'&&c<='9') { sum = sum * 10 + c - '0'; c = getchar(); }
+	return sum * ff;
 }
-void NTT(long long *A,int type)//NTT板子
-{
-    for(int i=0;i<limit;i++)
-    {
-        if(i<r[i])
-        {
-            swap(A[i],A[r[i]]);
-        }
-    }
-    for(int mid=1;mid<limit;mid<<=1)
-    {
-        long long Wn=fastpow(type==1?G:Gi,(mod-1)/(mid<<1));
-        for(int j=0;j<limit;j+=(mid<<1))
-        {
-            long long w = 1;
-            for(int k = 0; k < mid; k++, w = (w * Wn) % mod)
-            {
-                 int x=A[j+k];
-                 int y=w*A[j+k+mid]%mod;
-                 A[j+k]=(x+y)%mod,
-                 A[j+k+mid]=(x-y+mod)%mod;
-            }
-        }
-    }
-    if(type==-1)
-    {
-        long long inv =fastpow(limit,mod-2);
-        for(int i=0;i<=n+m;i++)
-        {
-           A[i]=(A[i]*inv)%mod;
-        }
-    }
+
+inline ll ksm(ll aa,ll bb) {
+	ll sum = 1;
+	while(bb) {
+		if(bb&1) sum = sum * aa %p;
+		bb >>= 1; aa = aa * aa %p;
+	}
+	return sum;
 }
-void poly_mul(long long *a,long long *b,int deg)//多项式乘法
-{
-    while(limit<=deg)
-    {
-        limit<<=1;
-        L++;
-    }
-    for(int i=0;i<limit;i++)
-    {
-        r[i]=(r[i>>1]>>1)|((i&1)<<(L-1));
-    }
-    NTT(a,1);
-    NTT(b,1);
-    for(int i=0;i<limit;i++)
-    {
-        a[i]=(a[i]*b[i])%mod;
-    }
-    NTT(a, -1);
+
+inline ll init(ll wo) {
+	ll len = 1, L = 0; while(len<wo) len<<=1, L++;
+	for(ll i=0;i<len;i++) r[i] = (r[i>>1]>>1) | ((i&1)<<(L-1));
+	return len;
 }
-int main()
-{
-    cin>>n>>m;
-    for(int i = 0; i <= n; i++)
-    {
-        cin>>a[i];
-        a[i]=(a[i]+mod)%mod;
-    }
-    for(int i = 0; i <= m; i++)
-    {
-        cin>>b[i];
-        b[i]=(b[i]+mod)%mod;
-    }
-    poly_mul(a,b,n+m);
-    for(int i=0;i<=n+m;i++)
-    {
-        cout<<a[i]<<" ";
-    }
-    cout<<endl;
-    return 0;
+
+void NTT(ll *A,ll len,ll cl) {
+	for(ll i=0;i<len;i++) if(i<r[i]) swap(A[i],A[r[i]]);
+	for(ll k=1;k<len;k<<=1) {
+		ll g1 = ksm((cl==1)?g:gi, (p-1)/(k<<1));
+		for(ll j=0;j<len;j+=(k<<1)) {
+			ll gk = 1;
+			for(ll i=0;i<k;i++,gk=gk*g1%p) {
+				ll x = A[i+j], y = gk*A[i+j+k] %p;
+				A[i+j] = (x+y) %p; A[i+j+k] = (x-y+p) %p;
+			}
+		}
+	}
+	if(cl==1) return ;
+	ll inv = ksm(len,p-2);
+	for(ll i=0;i<len;i++) A[i] = A[i] * inv %p;
+}
+
+void PMUL(ll *F,ll *G,ll len) {
+	NTT(F, len, 1);
+	NTT(G, len, 1);
+	for(ll i=0;i<len;i++) F[i] = F[i] * G[i] %p;
+	NTT(F, len, -1);
 }
 
