33acddf836e3ec8db6aca9fd1c47e2f79427c562
xiahouijhj <120567551+xiahouijhj@users.noreply.github.com>
2026-04-08 14:07:40 +0800

    Update tarjan无向图.cpp

--- 图论/tarjan无向图.cpp
+++ 图论/tarjan无向图.cpp
@@ -1,1 +1,22 @@
-
+void tarjan(int u,int fa)
+{
+	dfn[u]=low[u]=++t;
+	int child=0;
+	for(int i=head[u];i!=0;i=e[i].next)
+	{
+		int a=e[i].to;
+        //if(a==fa) continue;
+		if(!dfn[a])
+		{
+			tarjan(a,fa);
+			low[u]=min(low[u],low[a]);
+			if(low[a]>=dfn[u]&&u!=fa) cut[u]=1;
+			if(u==fa) child++;
+		}
+		low[u]=min(low[u],dfn[a]);
+	}
+	if(child>=2&&u==fa)
+	{
+		cut[u]=1;
+	}
+}
